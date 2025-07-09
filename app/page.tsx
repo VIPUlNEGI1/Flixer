feb3813f36d5b13b427d2b6fb96ad55d6799294b
vipulnehi <99vipul88@gmail.com>
2025-07-09 11:51:56 +0530

    7commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
+import { Suspense } from "react"
 import ServicesSection from "@/components/Services";
 import IndustriesSection from "@/components/IndustriesSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
@@ -11,7 +12,10 @@ export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <Suspense fallback={<div>Loading...</div>}>
       <TrustedBrand/>
+   
+    </Suspense>
       {/* <ServicesSection/>
       <IndustriesSection/>
       <WhyChooseUsSection/>
